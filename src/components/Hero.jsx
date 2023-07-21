24faac573aea1e1b0633bc11f1ac570b16ee01a1
Anselmo Rivera III <rivera.anselmo.2001@gmail.com>
2023-07-22 00:13:17 +0700

    implement reponsive layout to hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,30 +1,31 @@
 import HeroStyle from "../style/HeroStyle.css"
 import githubIcon from "../assets/images/github-icon.png"
 import linkedinIcon from "../assets/images/linkedin-icon.png"
+import user from "../assets/images/user.jpg"
 
 const Hero = () => {
     return (
-        <div className="hero--wrapper">
+        <section className="hero-section">
             <div className="container heroLayout">
-                <div>
-                    <h1 className="hero--heading leagueSpartanFont">Hi, <span className="mainColor">I'm Anselmo</span></h1>
-                    <h3 className="hero--subheading leagueSpartanFont">Javascript Fullstack Developer</h3>
-                    <div className="hero--desc futuraPtMd">A Highly motivated and passionate <span className="mainColor">Web Developer </span>
+                <div className="hero-user-desc">
+                    <h1 className="hero-heading leagueSpartanFont">Hi, <span className="mainColor">I'm Anselmo</span></h1>
+                    <h3 className="hero-subheading leagueSpartanFont">Javascript Fullstack Developer</h3>
+                    <div className="hero-desc futuraPtMd">A Highly motivated and passionate <span className="mainColor">Web Developer </span>
                         who creating interactive web applications and experiences
                         on the web
                     </div>
-                    <div className="hero--icons">
-                        <span><img className="hero--icon" src={linkedinIcon} alt="githubIcon" /></span>
-                        <span><img className="hero--icon" src={githubIcon} alt="linkedinIcon"/></span>
+                    <div className="hero-icons">
+                        <a href="https://www.linkedin.com/in/anselmo-rivera-iii" target="_blank"><span><img className="hero-icon" src={linkedinIcon} alt="githubIcon" /></span></a>
+                        <a href="https://www.github.com/anselmo07-2001"><span><img className="hero-icon" src={githubIcon} alt="linkedinIcon"/></span></a>
                     </div>
                 </div>
                 <div className="profileContainer">
                     <div className="profile mainColor">
-                        <div></div>
+                        <img src={user} className="profile-img"/>
                     </div>
                 </div>
             </div>
-        </div>
+        </section>
     )
 }
 
